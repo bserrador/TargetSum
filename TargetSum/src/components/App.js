1ebf13ee03cf9d6cc7511dc61dcb1a0c336ed424
Brittany Serrador <brittanyserrador@gmail.com>
2018-02-13 09:41:20 -0500

    Added a Play Again button
    Game resets when Play Again is pressed.

--- TargetSum/src/components/App.js
+++ TargetSum/src/components/App.js
@@ -3,9 +3,27 @@ import React from 'react';
 import Game from './Game';
 
 class App extends React.Component {
+    state = {
+        gameId: 1,
+    };
+
+    // Will unmount Game component & remount it with a new Id
+    resetGame = () => {
+        this.setState((prevState) => {
+            return { gameId: prevState.gameId + 1};
+        });
+    };
+
     render() {
         return (
-            <Game randomNumberCount={6} initialSeconds={10} />
+            // Associate Game with a key, if we give it a new key it will think
+            // it's a new game & will reset
+            <Game 
+                key={this.state.gameId} 
+                onPlayAgain={this.resetGame}
+                randomNumberCount={6} 
+                initialSeconds={10} 
+            />
         );
     }
 }
